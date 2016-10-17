7d82a8cbd0eeb82317740a69e021b2c3a87c844d
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2016-10-18 00:20:29 +0800

    updated gui and duummy data

--- AttendanceChecker/app/src/main/java/com/example/avggo/attendancechecker/model/TutorialData.java
+++ AttendanceChecker/app/src/main/java/com/example/avggo/attendancechecker/model/TutorialData.java
@@ -10,30 +10,20 @@ import java.util.List;
  */
 
 public class TutorialData {
-    private static final String[] titles = {"Nothingness cannot be defined",
-            "Time is like a river made up of the events which happen, and a violent stream; " +
-                    "for as soon as a thing has been seen, it is carried away, and another comes" +
-                    " in its place, and this will be carried away too,",
-            "But when I know that the glass is already broken, every minute with it is precious.",
-            "For me, it is far better to grasp the Universe as it really is than to persist in" +
-                    " delusion, however satisfying and reassuring.",
-            "The seeker after the truth is not one who studies the writings of the ancients and," +
-                    " following his natural disposition, puts his trust in them, but rather the" +
-                    " one who suspects his faith in them and questions what he gathers from them," +
-                    " the one who submits to argument and demonstration, and not to the " +
-                    "sayings of a human being whose nature is fraught with all kinds " +
-                    "of imperfection and deficiency.",
-            "You must take personal responsibility. You cannot change the circumstances, the" +
-                    " seasons, or the wind, but you can change yourself. That is something you" +
-                    " have charge of."
+    private static final String[] titles = {"G202",
+            "G203",
+            "A909",
+            "G201",
+            "G204",
+            "G206"
     };
-    private static final String[] subTitles = {"Bruce Lee",
-            "Marcus Aurelius",
-            "Meng Tzu",
-            "Ajahn Chah",
-            "Carl Sagan",
-            "Alhazen",
-            "Jim Rohn"
+    private static final String[] subTitles = {"Ms. Kyle-Althea Santos",
+            "Mr. Vince Gonzales",
+            "Mr. Junlyn Alburo",
+            "Mr. Jarrell Chua",
+            "Mr. Winfred Villaluna",
+            "Mr. Dhiren Dhanani",
+            "Ms. Darlene Marpa"
 
     };
     private static final int icon = R.drawable.ic_tonality_black_36dp;
